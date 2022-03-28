ac97d5354f80c03113e7cd68458040056e7179d9
Rachel Tairum <rachel.tairum@gmail.com>
2022-03-28 20:14:43 -0300

    Tela de Perfil: Finaliza redirecionamentos e limpa storage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { shape } from 'prop-types';
 
-function Profile() {
+function Profile({ history }) {
   const userEmail = JSON.parse(localStorage.user);
+
+  const clickLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div>
       <h2>Profile</h2>
       <h3 data-testid="profile-email">{userEmail.email}</h3>
-      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
-      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
-      <button type="button" data-testid="profile-logout-btn">Logout</button>
+      <Link to="/done-recipes">
+        <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      </Link>
+      <Link to="/favorite-recipes">
+        <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      </Link>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ clickLogout }
+      >
+        Logout
+      </button>
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: shape,
+}.isRequired;
 
 export default Profile;
