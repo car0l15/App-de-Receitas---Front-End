4e4fcc031cfe100a4231dcc76946e860698a5adf
Rachel Tairum <rachel.tairum@gmail.com>
2022-03-28 19:33:09 -0300

    Tela de Perfil: insere elementos na tela

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
 function Profile() {
+  const userEmail = JSON.parse(localStorage.user);
   return (
-    <h2>Profile</h2>
+    <div>
+      <h2>Profile</h2>
+      <h3 data-testid="profile-email">{userEmail.email}</h3>
+      <button type="button" data-testid="profile-done-btn">Done Recipes</button>
+      <button type="button" data-testid="profile-favorite-btn">Favorite Recipes</button>
+      <button type="button" data-testid="profile-logout-btn">Logout</button>
+    </div>
   );
 }
 
