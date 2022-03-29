f34e9de1b59082d0720149b5c71fd25582dd4d4c
Yuri Carvalho <yuricps@gmail.com>
2022-03-29 20:11:47 -0300

    Requisito 16 OK :heavy_check_mark:

--- src/pages/Drinks.jsx
+++ src/pages/Drinks.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 import LowerMenu from '../components/LowerMenu';
+import { MyContext } from '../context/Provider';
 
 function Drinks() {
+  const { drinkList } = useContext(MyContext);
   return (
     <div>
       <h2>Drinks</h2>
       <Header />
+      {drinkList.length === 1 && <Redirect to={ `/drinks/${drinkList[0].idDrink}` } />}
       <LowerMenu />
     </div>
   );
