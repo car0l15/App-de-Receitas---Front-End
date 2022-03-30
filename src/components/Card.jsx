68878d329c73bcd00931ca1950f839023f305870
Yuri Carvalho <yuricps@gmail.com>
2022-03-29 23:16:23 -0300

    Requisito 17 OK :heavy_check_mark:

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { string, number } from 'prop-types';
 
 function Card({ name, img, index }) {
   return (
-    <div key={ index } data-testid={ `${index}-recipe-card` }>
+    <div data-testid={ `${index}-recipe-card` }>
       <img
         data-testid={ `${index}-card-img` }
         src={ img }
         alt={ name }
+        width="150"
       />
       <p data-testid={ `${index}-card-name` }>{name}</p>
     </div>
   );
 }
 
-Card.propTypes = {
-  index: PropTypes.number.isRequired,
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-};
-
 export default Card;
+Card.propTypes = {
+  name: string,
+  img: string,
+  index: number,
+}.isRequired;
