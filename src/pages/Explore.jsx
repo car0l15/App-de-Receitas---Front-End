97d75cdd97983d828f93c8110d0745e0d08e997f
Rachel Tairum <rachel.tairum@gmail.com>
2022-03-31 17:19:52 -0300

    Requisitos 67 e 68: OK

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -7,6 +7,8 @@ function Explore() {
     <div>
       <h2>Explore</h2>
       <Header />
+      <button type="button" data-testid="explore-foods">Explore Foods</button>
+      <button type="button" data-testid="explore-drinks">Explore Drinks</button>
       <LowerMenu />
     </div>
   );
