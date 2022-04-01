cb694f10452abdd2fc0afda295356ac45f5915d0
Yuri Carvalho <yuricps@gmail.com>
2022-04-01 17:40:57 -0300

    Requisito 33(Tags e TestIDs) OK :heavy_check_mark:

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,7 +2,36 @@ import React from 'react';
 
 function Details() {
   return (
-    <h2>Details</h2>
+    <>
+      <h2>Details</h2>
+      <img data-testid="recipe-photo" alt="Recipe Img" />
+      <h3 data-testid="recipe-title">Title</h3>
+      <h5 data-testid="recipe-category">Category</h5>
+      <button
+        data-testid="share-btn"
+        type="button"
+      >
+        Share
+      </button>
+      <button
+        data-testid="favorite-btn"
+        type="button"
+      >
+        Favorite
+      </button>
+      <ul>
+        <li data-testid="0-ingredient-name-and-measure">Ingredient</li>
+      </ul>
+      <p data-testid="instructions">Instructions</p>
+      <iframe data-testid="video" width="320" height="180" src="https://www.youtube.com/embed/lJIrF4YjHfQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" />
+      <div data-testid="0-recomendation-card">Recommendations</div>
+      <button
+        data-testid="start-recipe-btn"
+        type="button"
+      >
+        Start Recipe
+      </button>
+    </>
   );
 }
 
