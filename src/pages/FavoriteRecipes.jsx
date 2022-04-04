31c92a62c8a89610f9a148a2cc8ce5c777bed15b
Caroline Nepomuceno <carol14898@gmail.com>
2022-04-04 17:03:29 -0300

    salvando alterações

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 
 function FavoriteRecipes() {
+  const [favoriteRecipesList, setFavoriteRecipesList] = useState([]);
+
+  useEffect(() => {
+    setFavoriteRecipesList(JSON.parse(localStorage.favoriteRecipes));
+  }, []);
+
+  console.log(favoriteRecipesList, 'listFavorites');
+
+  // a foto da receita, o nome, a categoria, a nacionalidade
+
   return (
-    <>
+    <div>
       <h2>FavoriteRecipes</h2>
       <Header />
-    </>
+
+      { favoriteRecipesList.map((favoriteItem) => (
+        <div key={ favoriteItem.id }>
+          <img src={ favoriteItem.image } alt="imagem do item favoritado" width="100" />
+          <p>{ favoriteItem.name }</p>
+          <p>{ favoriteItem.category }</p>
+          { favoriteItem.nationality === ''
+            ? <p>{ favoriteItem.alcoholicOrNot }</p>
+            : <p>{ favoriteItem.na }</p> }
+
+          <button
+            type="button"
+          >
+            Compartilhar
+          </button>
+
+          <button
+            type="button"
+          >
+            Desfavoritar
+          </button>
+
+        </div>
+      )) }
+    </div>
   );
 }
 
